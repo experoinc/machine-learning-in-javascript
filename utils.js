ea2a47504f75a2a97fccce652f71ec83366cdc04
Steve Purves <stevejpurves@gmail.com>
2017-09-30 21:56:29 +0100

    kmeans & confusion

--- utils.js
+++ utils.js
@@ -2,7 +2,15 @@ var fs = require("fs");
 var parse = require('csv-parse/lib/sync');
 var table = require('text-table');
 
-function loadUnlabelledWine(verbose) {
+function loadUnlabelledWine(options) {
+    return loadWine("datasets/wine.data.unlabelled.csv", options);
+}
+    
+function loadLabelledWine(options) {
+    return loadWine("datasets/wine.data.csv", options);
+}
+    
+function loadWine(filename, options) {
     
     var features = [
     'Alcohol',
@@ -17,12 +25,13 @@ function loadUnlabelledWine(verbose) {
     'Color intensity',
     'Hue',
     'OD280/OD315 of diluted wines',
-    'Proline'];
+    'Proline',
+    'Class'];
 
-    var raw_csv_string = fs.readFileSync("datasets/wine.data.unlabelled.csv").toString();
-    var dataset = parse(raw_csv_string);       
+    var raw_csv_string = fs.readFileSync(filename).toString();
+    var dataset = parse(raw_csv_string, {auto_parse: true});       
     var format = { align: [ 'c', 'c' ], hsep: ' | ' };
-    if (verbose) {       
+    if (options && options.verbose) {       
         console.log("our dataset has", dataset.length, "rows and ", dataset[0].length, "columns")
         var T = table([features], format);
         console.log(T)
@@ -37,6 +46,21 @@ function loadUnlabelledWine(verbose) {
     }
 }
 
+function grid2(range=[-1,1], step=0.01) {
+    var X = [];
+
+    // build a 2d array of coordinates
+    for (var i = range[0]; i <= range[1]; i += step) {
+        var x = [];
+        for (var j = range[0]; j <= range[1]; j += step) {
+            x.push([i, j])
+        }
+        X.push(x)
+    }
+}
+
 module.exports = {
-    loadUnlabelledWine
+    loadUnlabelledWine,
+    loadLabelledWine,
+    grid2
 };
